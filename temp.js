b8ef20bf1610408f4b4950c272081f3c694f35dd
Shashidhar Angadi <shashidharangadi14@gmail.com>
2024-02-21 00:54:37 +0530

    MongoDB_Integration

--- temp.js
+++ temp.js
@@ -27,3 +27,12 @@ console.log(original2);
 const  array = [1,2,34,5];
 array.splice(2,1);
 console.log(array);
+
+const obje = [{'first':1,'third':3},{'second':2}]
+async function fun()
+{
+    const result = await obje.find({});
+    console.log('Result ',result);
+}
+fun();
+
